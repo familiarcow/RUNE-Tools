738016a5ef1f4bf6650439794fd3c2d039859f8d
cow9r <familiarcow@proton.me>
2025-03-12 21:23:23 -0400

    Add operator outlink

--- src/lib/Nodes.svelte
+++ src/lib/Nodes.svelte
@@ -735,7 +735,15 @@ const ispToImage = {
                 {node.bond_providers?.providers?.length || 0}
               </span>
             </td>
-            <td class="monospace">{node.node_operator_address.slice(-4)}</td>
+            <td class="monospace">{node.node_operator_address.slice(-4)}
+              <a href="https://runescan.io/address/{node.node_operator_address}" target="_blank" rel="noopener noreferrer" class="outlink">
+                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                  <polyline points="15 3 21 3 21 9"></polyline>
+                  <line x1="10" y1="14" x2="21" y2="3"></line>
+                </svg>
+              </a>
+            </td>
             {#if node.isp}
               <td>
                 <span class="isp">
@@ -1016,7 +1024,15 @@ const ispToImage = {
                 {node.bond_providers?.providers?.length || 0}
               </span>
             </td>
-            <td class="monospace">{node.node_operator_address.slice(-4)}</td>
+            <td class="monospace">{node.node_operator_address.slice(-4)}
+              <a href="https://runescan.io/address/{node.node_operator_address}" target="_blank" rel="noopener noreferrer" class="outlink">
+                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                  <polyline points="15 3 21 3 21 9"></polyline>
+                  <line x1="10" y1="14" x2="21" y2="3"></line>
+                </svg>
+              </a>
+            </td>
             {#if node.isp}
               <td>
                 <span class="isp">
